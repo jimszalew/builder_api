59af2ced4ea1b3994d2e59482799e34fdf3824ad
Jim Szalewski <jim.szalewski@gmail.com>
2018-01-23 10:10:41 -0700

    initialize cardsearch

--- app/models/card_search.rb
+++ app/models/card_search.rb
@@ -1,34 +1,45 @@
 class CardSearch
-  def self.finder(params)
-    by_color = []
-    by_types = []
-    if params['colors'].present?
-      by_color = self.cards_by_color(params['colors'])
-    end
-    if params['types'].present?
-      by_types = self.cards_by_types(params['types'])
-    end
-    (by_color + by_types).uniq
+
+  attr_reader :colors, :types
+
+  def initialize(params)
+    @colors = params[:colors]
+    @types  = params[:types]
   end
 
-  def self.cards_by_color(colors)
-    colors_array = colors.split(',')
-    all_combos = (1..5).flat_map{|n| colors_array.repeated_permutation(n).map(&:join)}
-    all_combos.delete_if { |combo| combo.split(//).uniq.length != combo.split(//).length }
-    uniq_combos = all_combos.map { |combo| combo.split(//).sort }.uniq.map { |combo| combo.join(',') }
-    cards = []
-    uniq_combos.map do |color|
-      cards << Card.where(color_id: color)
-    end
-    cards
-  end
-
-  def self.cards_by_types(types)
-    card_types = types.split(',')
-    cards = []
-    card_types.each do |tipe|
-      cards << Card.where("card_types LIKE ?", "%#{tipe}%")
-    end
-    cards[0].uniq
-  end
 end
+
+# class CardSearch
+#   def self.finder(params)
+#     by_color = []
+#     by_types = []
+#     if params['colors'].present?
+#       by_color = self.cards_by_color(params['colors'])
+#     end
+#     if params['types'].present?
+#       by_types = self.cards_by_types(params['types'])
+#     end
+#     (by_color + by_types).uniq
+#   end
+#
+#   def self.cards_by_color(colors)
+#     colors_array = colors.split(',')
+#     all_combos = (1..5).flat_map{|n| colors_array.repeated_permutation(n).map(&:join)}
+#     all_combos.delete_if { |combo| combo.split(//).uniq.length != combo.split(//).length }
+#     uniq_combos = all_combos.map { |combo| combo.split(//).sort }.uniq.map { |combo| combo.join(',') }
+#     cards = []
+#     uniq_combos.map do |color|
+#       cards << Card.where(color_id: color)
+#     end
+#     cards
+#   end
+#
+#   def self.cards_by_types(types)
+#     card_types = types.split(',')
+#     cards = []
+#     card_types.each do |tipe|
+#       cards << Card.where("card_types LIKE ?", "%#{tipe}%")
+#     end
+#     cards[0].uniq
+#   end
+# end
---
+++ spec/models/card_search_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe CardSearch, type: :model do
+
+  describe 'initialize' do
+    it 'creates a CardSearch object with attributes' do
+      params = {colors: "W,U,B", types: "Creature"}
+      cards = CardSearch.new(params)
+
+      expect(cards.class).to eq(CardSearch)
+      expect(cards.colors).to eq("W,U,B")
+      expect(cards.types).to eq("Creature")
+    end
+  end
+
+  describe 'excluded_colors' do
+    it 'returns colors excluded from the search params' do
+
+    end
+  end
+end
