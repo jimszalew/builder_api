30bafc3c1acfd37be85da4b504f0b0c6dd8572a0
Jim Szalewski <jim.szalewski@gmail.com>
2018-01-25 16:05:58 -0700

    change to instance variables in search controllers

--- app/controllers/api/v1/card_search_controller.rb
+++ app/controllers/api/v1/card_search_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::CardSearchController < ApplicationController
   def index
-    card_search = CardSearch.new(params)
-    render json: card_search.finder
+    @card_search = CardSearch.new(params).finder
+    render json: @card_search
   end
 end
